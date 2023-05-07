93eedcae5833ac348e581154113ab04b96e9c1d2
homeworkdome <homeworkdome@outlook.com>
2023-05-07 15:31:40 +0300

    Добавьте файлы проекта.

---
+++ RevitAPITrainingIFC/Main.cs
@@ -0,0 +1,38 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITrainingIFC
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+
+            using (var ts = new Transaction(doc, "export ifc"))
+            {
+                ts.Start();
+
+                ViewPlan viewPlan = new FilteredElementCollector(doc)
+                                        .OfClass(typeof(ViewPlan))
+                                        .Cast<ViewPlan>()
+                                        .FirstOrDefault(v => v.ViewType == ViewType.FloorPlan &&
+                                                           v.Name.Equals("Level 1"));
+                var ifcOption = new IFCExportOptions();
+                doc.Export(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "export.ifc", ifcOption);
+                ts.Commit();
+            }
+
+            return Result.Succeeded;
+        }
+
+       
+    }
+    }
